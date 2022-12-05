aad83202074a2ccfda3a84894750a2e3bdc6ccba
huaqo <mail@huaqo.com>
2022-12-05 17:04:49 +0100

    skript

---
+++ skript.R
@@ -0,0 +1,16 @@
+#### Install packages ####
+# install.packages("terra")
+
+#### Acticate packages ####
+library(terra) # Spatial Data Analysis
+
+#### Import raster files ####
+blue <- rast("raster1.TIF")
+green <- rast("raster2.TIF")
+red <- rast("raster3.TIF")
+
+#### Create layerstack ####
+layerstack <- c(blue, green, red)
+
+#### Plot composite ####
+plotRGB(layerstack, 3, 2, 1, stretch = "lin")
